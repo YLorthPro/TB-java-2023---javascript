d30e4a7a6e8ad4c1a3de521d605301a3ee4cbc26
YLorthPro <yann.lorthioir@bstorm.be>
2023-09-07 11:56:54 +0200

    is NAN

--- Exercices Formulaire/Exo 2/Exo2.js
+++ Exercices Formulaire/Exo 2/Exo2.js
@@ -19,9 +19,9 @@ function codePostal(){
     //1. Verifier si pas de champs vide
     if(document.formulaire.prenom.value == "" || document.formulaire.nom.value == "" || document.formulaire.codePostal.value == "")
         alert("Les champs ne peuvent pas être vide");
-    
+
     //2. Verifier si le code postal est compris entre 1000 et 9999
-    else if (document.formulaire.codePostal.value < 1000 || document.formulaire.codePostal.value >9999)
+    else if (document.formulaire.codePostal.value < 1000 || document.formulaire.codePostal.value >9999 || isNaN(document.formulaire.codePostal.value))
         alert("Ce n'est pas un nombre valide");
     // Formulaire valide
     else
