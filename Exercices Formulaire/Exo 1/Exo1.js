be1cdaafa9b6e49d357335d9e4f81ffeac6bd88c
YLorthPro <yann.lorthioir@bstorm.be>
2023-09-07 11:55:19 +0200

    Exo formulaire

---
+++ Exercices Formulaire/Exo 1/Exo1.js
@@ -0,0 +1,9 @@
+function remplissage(){
+    let f = document.formulaire;
+    f.prenom.value = "Jean";
+    f.nom.value = "Dupont";
+};
+
+function reset(){
+    document.formulaire.reset();
+};
---
+++ Exercices Formulaire/Exo 2/Exo2.js
@@ -0,0 +1,29 @@
+function remplissage(){
+    //modification de l'input "prenom" dans le form "formulaire"
+    document.formulaire.prenom.value = "Jean";
+
+    //modification de l'input "nom" dans le form "formulaire"
+    document.formulaire.nom.value = "Dupont";
+
+    //modification de l'input "codePostal" dans le form "formulaire"
+    document.formulaire.codePostal.value="1234";
+    };
+   
+function reset(){
+    //Reset du form "formulaire"
+    document.formulaire.reset();
+    };
+
+function codePostal(){
+
+    //1. Verifier si pas de champs vide
+    if(document.formulaire.prenom.value == "" || document.formulaire.nom.value == "" || document.formulaire.codePostal.value == "")
+        alert("Les champs ne peuvent pas être vide");
+    
+    //2. Verifier si le code postal est compris entre 1000 et 9999
+    else if (document.formulaire.codePostal.value < 1000 || document.formulaire.codePostal.value >9999)
+        alert("Ce n'est pas un nombre valide");
+    // Formulaire valide
+    else
+        alert("Formulaire envoyé")
+};
